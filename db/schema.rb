ec9cafe49f8d6d3e9fea80a22d3f14d5e80567ad
Marco Colombo <mcolom4@uillinois.edu>
2025-04-28 16:41:17 -0500

    add pgcrypto extension: Azure flexible db issue

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_26_144056) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   # enable_extension "pg_catalog.plpgsql"
+  enable_extension 'pgcrypto'
   enable_extension "uuid-ossp"
 
   create_table "action_mailbox_inbound_emails", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
