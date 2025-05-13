296a597e7aa4f783f9a49722222c88bb41a10376
Ian Heraty <heratyian@gmail.com>
2025-05-13 15:14:34 -0500

    Fix broken tests, remove unneeded tests.

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -17,13 +17,4 @@ RSpec.describe Resource, type: :model do
       expect(resource.project_path).to eq('testuser/test-repo')
     end
   end
-
-  describe '#to_s' do
-    let(:resource) { build(:resource, project_url: 'https://github.com/testuser/test-repo') }
-
-    it 'returns a string representation with ID and project path' do
-      allow(resource).to receive(:id).and_return('123')
-      expect(resource.to_s).to eq('Resource #123: testuser/test-repo')
-    end
-  end
 end
